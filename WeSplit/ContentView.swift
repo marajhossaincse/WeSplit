add869dcc1f6c568b37462fd15967dc5d2f1f683
Maraj Hossain <marajhossaincse@gmail.com>
2023-03-31 20:28:07 +0600

    first commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Maraj Hossain on 3/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var totalAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = totalAmount/100 * tipSelection
+        let grandTotal = totalAmount + tipValue
+
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField(
+                        "Enter Amount", value: $totalAmount, format:
+                        .currency(code: Locale.current.currency?.identifier ?? "BDT")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people.")
+                        }
+                    }
+                }
+
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip would you like to leave?")
+                }
+
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "BDT"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Maraj Hossain on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
